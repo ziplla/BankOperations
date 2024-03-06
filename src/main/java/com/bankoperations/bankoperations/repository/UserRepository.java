15904fe06cb2249bcc572b74a9f273f2ebacf441
Pavel <pzajtsev@yandex.ru>
2024-03-06 19:31:03 +0400

    more methods

--- src/main/java/com/bankoperations/bankoperations/repository/UserRepository.java
+++ src/main/java/com/bankoperations/bankoperations/repository/UserRepository.java
@@ -6,4 +6,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByPhoneNumber(String phoneNumber);
+
+    User findByEmail(String email);
+
+    User findByUsername(String username);
 }
